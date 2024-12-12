a416d246ba61c7a5dda97ccc9c14d85e6731ddde
Amr69130 <amrouchenorris@gmail.com>
2024-12-12 15:33:51 +0100

    add script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const closeButton = document.getElementById('closeButton');
+const dialogBox = document.getElementById('dialogBox');
+
+
+closeButton.addEventListener('click', () => {
+  dialogBox.style.display = 'none'; 
+});
+
+
+function updateClock() {
+  const date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  hours = hours < 10 ? '0' + hours : hours;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  const time = hours + ':' + minutes;
+  document.getElementById('clock').textContent = time;
+}
+
+
+setInterval(updateClock, 1000);
+updateClock(); 
