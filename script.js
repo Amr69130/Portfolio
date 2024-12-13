2203457bc167f3610d05a2cc01ac0851ddfbce9f
Amr69130 <amrouchenorris@gmail.com>
2024-12-13 16:48:17 +0100

    add pics

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
+// Gestion de la fermeture de la boîte de dialogue
+const closeButton = document.getElementById("closeButton");
+const dialogBox = document.getElementById("dialogBox");
 
-const closeButton = document.getElementById('closeButton');
-const dialogBox = document.getElementById('dialogBox');
-
-
-closeButton.addEventListener('click', () => {
-  dialogBox.style.display = 'none'; 
+closeButton.addEventListener("click", () => {
+  dialogBox.style.display = "none";
 });
 
-
+// Mise à jour de l'horloge en temps réel
 function updateClock() {
   const date = new Date();
   let hours = date.getHours();
   let minutes = date.getMinutes();
-  hours = hours < 10 ? '0' + hours : hours;
-  minutes = minutes < 10 ? '0' + minutes : minutes;
-  const time = hours + ':' + minutes;
-  document.getElementById('clock').textContent = time;
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  const time = hours + ":" + minutes;
+  document.getElementById("clock").textContent = time;
 }
 
-// PERMET DE LANCER LA FONCTION 1ER PARAMETRE 
-setInterval(updateClock, 1000); 
-updateClock(); 
+// Met à jour l'horloge chaque seconde
+setInterval(updateClock, 1000);
+updateClock();
