62fb94eb61252bf40ba6ac8aed3b43358794d06d
Amr69130 <amrouchenorris@gmail.com>
2024-12-19 14:42:33 +0100

    add song

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
-
 const closeButton = document.getElementById("closeButton");
 const welcomeWrapper = document.getElementById("welcomeWrapper");
+const errorSound = document.getElementById("errorSound");  
 
 closeButton.addEventListener("click", () => {
   welcomeWrapper.style.display = "none";
-  welcomeWrapperEmpty.style.display = "inline-block"
-  
+  welcomeWrapperEmpty.style.display = "inline-block";
 });
-
-
 
 
 setTimeout(() => {
   errorBox.style.display = "block";
-}, 15000);
+  errorSound.play(); 
+}, 20000);
 
 const closeButtonError = document.getElementById("closeButtonError");
 const errorBox = document.getElementById("errorBox");
 
 closeButtonError.addEventListener("click", () => {
   errorBox.style.display = "none";
+
+  
   setTimeout(() => {
     errorBox.style.display = "block";
-  }, 3000);
-  
+    errorSound.play();  
+  }, 2000);
 });
-
-
-
-
 
 function updateClock() {
   const date = new Date();
@@ -39,7 +35,6 @@ function updateClock() {
   const time = hours + ":" + minutes;
   document.getElementById("clock").textContent = time;
 }
-
 
 setInterval(updateClock, 1000);
 updateClock();
